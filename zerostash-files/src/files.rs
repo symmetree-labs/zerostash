aa23b7222829385f830f49961840952259175737
rsdy <p@symmetree.dev>
2022-05-14 18:05:57 +0100

    Add `open_symlink`, hope it's correct

--- zerostash-files/src/files.rs
+++ zerostash-files/src/files.rs
@@ -155,7 +155,6 @@ impl Entry {
         path: &impl AsRef<Path>,
         preserve: &PreserveMetadata,
     ) -> Result<Entry, EntryError> {
-        let path = path.as_ref();
         let (unix_secs, unix_nanos) = if preserve.times {
             to_unix_mtime(&metadata)?
         } else {
@@ -217,7 +216,7 @@ impl Entry {
             },
 
             size: metadata.len(),
-            name: normalize_filename(&path)?,
+            name: normalize_filename(path)?,
 
             chunks: Vec::new(),
         })
@@ -304,6 +303,34 @@ impl Entry {
         } else {
             None
         })
+    }
+}
+
+#[cfg(windows)]
+fn open_symlink(
+    path: impl AsRef<Path> + Copy,
+    pointed_to: impl AsRef<Path> + Copy,
+) -> Result<fs::File, io::Error> {
+    use std::os::windows::fs::{symlink_dir, symlink_file};
+
+    let pointed_md = std::fs::metadata(pointed_to)?;
+    let symlink = if pointed_md.is_dir() {
+        symlink_dir
+    } else {
+        symlink_file
+    };
+
+    match symlink(pointed_to, path) {
+        Ok(()) => Ok(fs::OpenOptions::new().read(true).open(path)?),
+        Err(err) if err.kind() == io::ErrorKind::NotFound => {
+            if let Some(parent) = path.as_ref().parent() {
+                fs::create_dir_all(parent)?;
+                open_symlink(path, pointed_to)
+            } else {
+                Err(err)
+            }
+        }
+        Err(err) => Err(err),
     }
 }
 
