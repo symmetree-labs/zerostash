2a9b5a03158ed1649157a7f14c6602cd58e34236
almugra <almugra@>
2023-01-17 20:24:59 +0100

    added mount benchmark & cleaned up code

---
+++ zerostash/benches/bench.rs
@@ -0,0 +1,34 @@
+use std::{ffi::OsStr, path::PathBuf, sync::mpsc};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use fuse_mt::FuseMT;
+use infinitree::{backends, crypto::UsernamePassword, Infinitree};
+use zerostash::commands::ZerostashFS;
+
+criterion_group!(benches, mount_starup);
+criterion_main! {benches}
+
+fn mount_starup(c: &mut Criterion) {
+    c.bench_function("mount startup", |b| b.iter(mount));
+}
+
+fn mount() -> anyhow::Result<()> {
+    let (tx, finished) = mpsc::sync_channel(2);
+
+    let key = "abcde".to_string();
+    let key = UsernamePassword::with_credentials(key.clone(), key).unwrap();
+
+    let backend = backends::Directory::new(PathBuf::from("../tests/data/Mounting/Stash/")).unwrap();
+    let stash = Infinitree::open(backend, key).unwrap();
+    let fuse_args = [OsStr::new("-o"), OsStr::new("fsname=zerostash")];
+    let options = zerostash_files::restore::Options::default();
+    let filesystem = ZerostashFS::open(stash, &options, tx).unwrap();
+    let fs = FuseMT::new(filesystem, 1);
+    fuse_mt::spawn_mount(fs, "../tests/data/Mounting/Target/", &fuse_args[..])
+        .unwrap()
+        .join();
+
+    finished.recv().expect("Could not receive from channel.");
+
+    Ok(())
+}
